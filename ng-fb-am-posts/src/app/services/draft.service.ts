178b3440224493c6a620a0e7b743a0e1b7f2370b
Kyle Henry <krhenry433@gmail.com>
2019-02-02 14:50:02 -0500

    Added little bit of styling to draft component. Reordered menu list.

---
+++ ng-fb-am-posts/src/app/services/draft.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Captain } from '../models/captain.model';
+import { Team } from '../models/team.model';
+
+
+@Injectable()
+export class DraftService {
+  TeamList: AngularFireList<any>;
+  selectedTeam: Team = new Team();
+
+  constructor(private firebase: AngularFireDatabase ) { }
+
+  getData() {
+    this.TeamList = this.firebase.list('teams');
+    return this.TeamList;
+  }
+
+  insertTeam(clan, player) {
+    console.log(clan, player);
+    this.TeamList.push({
+      clan: clan,
+      player: player,
+      // color1: captain.color1,
+      // color2: captain.color2
+    });
+  }
+
+  updateTeam(key, clan) {
+    this.TeamList.update(key,
+      {
+        clan: clan,
+        // color1: captain.color1,
+        // color2: captain.color2
+      });
+  }
+
+  deleteTeam($key: string) {
+    this.TeamList.remove($key);
+  }
+
+  deleteAllTeams() {
+    this.TeamList.remove();
+  }
+
+}
